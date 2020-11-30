19a845a4b37ef563d42f705398ae6b10b63a1e79
mamane19 <b.moussaama@alustudent.com>
2020-11-30 18:57:59 +0200

    refactor:moved project

---
+++ project/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Main file of the pychecs2 package. It is this file that we will run to start your game.
+
+"""
+from pychecs2.echecs.game import Game
+from pychecs2.interface.interface import Window
+
+if __name__ == '__main__':
+    # Create a new Game
+    p = Game()
+
+    # Creation and display of a window (no link with the part above).
+    f = Window()
+    f.mainloop()
